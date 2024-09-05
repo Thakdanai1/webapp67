a96eaad66259d8efa9a0fc6ec1ece424de1ca409
Thakdanai1 <oom0955907823@gmail.com>
2024-09-05 15:17:19 +0700

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,7 @@
+console.log('this ismy svript file from java');
+console.log('hello thrsday');
+document.getElementById("text").innerHTML = "thakdanai Mooksri";
+document.write("hello document write");
+
+window.alert("welcom to my page");
+
