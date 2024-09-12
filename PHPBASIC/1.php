45c8465b7ad3f377f9842b8ddefa0251c2344fab
Thakdanai1 <oom0955907823@gmail.com>
2024-09-12 15:18:42 +0700

    Add files via upload

---
+++ PHPBASIC/1.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=<, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        echo "Hello PHP";
+        echo "<br>";
+        echo "this is myfirst time yo write PHP languad";
+    echo"<br>";
+    echo"<br>";
+    define("MINSIZE",50);
+        echo MINSIZE;
+
+    ?>
+</body>
+</html>
---
+++ PHPBASIC/function26.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>writeMessesage function</title>
+</head>
+<body>
+    <?php
+        function writeMessesage(){
+            echo "this is first function for write messesage";
+            echo"<br>";
+            echo "Have a nice program";
+
+        }//stop function
+
+        //call function
+        writeMessesage();
+    
+    
+    ?>
+</body>
+</html>
